56b4c1eb65e1b36b8dbd41b58d6f4a475c6b4f0f
dnjswns98 <tngkrt2004@gmail.com>
2025-07-09 02:53:42 +0900

    api연결 및 mygroup 조회

--- src/service/social/Group.js
+++ src/service/social/Group.js
@@ -1,10 +1,10 @@
-import axiosInstance from "./axiosInstance";
+import axiosInstance from "../axiosInstance";
 
 export default {
   // 그룹 생성
   async createGroup(name) {
     try {
-      await axiosInstance.post("/groups", { name });
+      await axiosInstance.post("/api/groups", { name });
       return "success";
     } catch (err) {
       console.log(err.response?.data?.message);
@@ -15,7 +15,7 @@ export default {
   // 그룹 이름 수정
   async updateGroupName(groupId, name) {
     try {
-      await axiosInstance.put(`/groups/${groupId}`, { name });
+      await axiosInstance.put(`/api/groups/${groupId}`, { name });
       return "success";
     } catch (err) {
       console.log(err.response?.data?.message);
@@ -26,7 +26,7 @@ export default {
   // 그룹 삭제
   async deleteGroup(groupId) {
     try {
-      await axiosInstance.delete(`/groups/${groupId}`);
+      await axiosInstance.delete(`/api/groups/${groupId}`);
       return "success";
     } catch (err) {
       console.log(err.response?.data?.message);
@@ -37,7 +37,7 @@ export default {
   // 내가 속한 그룹 목록 조회
   async fetchMyGroups() {
     try {
-      const response = await axiosInstance.get("/groups");
+      const response = await axiosInstance.get("/api/groups/me");
       return response.data;
     } catch (err) {
       console.log(err.response?.data?.message);
@@ -48,7 +48,7 @@ export default {
   // 특정 그룹 상세 조회
   async fetchGroupDetails(groupId) {
     try {
-      const response = await axiosInstance.get(`/groups/${groupId}`);
+      const response = await axiosInstance.get(`/api/groups/${groupId}`);
       return response.data;
     } catch (err) {
       console.log(err.response?.data?.message);
